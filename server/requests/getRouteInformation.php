3f966be3ac31216678c0b6f990449808db7d162f
Maury Quijada <maury.quijada@gmail.com>
2013-07-27 23:27:57 +0000

    Make skeleton functions.

---
+++ server/requests/getRouteInformation.php
@@ -0,0 +1,31 @@
+<?php
+
+class getRouteInformation
+{
+    public function __construct ()
+    {
+
+    }
+
+    public function getCaltrainInformation ()
+    {
+
+    }
+
+    public function getBARTInformation ()
+    {
+
+    }
+
+    public function getMUNIRailInformation ()
+    {
+
+    }
+
+    public function getWalkInformation ()
+    {
+
+    }
+}
+
+?>
