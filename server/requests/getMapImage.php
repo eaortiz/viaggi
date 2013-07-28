d64478b48976f553f2526daa8e6690fe3b15b570
JMoranC <janeth.moranc@gmail.com>
2013-07-27 21:58:00 -0700

    image url; getMapImage

--- server/requests/getMapImage.php
+++ server/requests/getMapImage.php
@@ -2,10 +2,26 @@
 
 class mapImage
 {
+    public $parameters;
+
     public function __construct ()
     {
-
+        $this->parameters = $_GET;
     }
+    
+    public function getMap()
+    {
+    	$url = 'http://maps.googleapis.com/maps/api/staticmap?'.
+    			'&zoom='.$this->parameters["zoomValue"]. 
+    			'&size=600x400&maptype=roadmap'.
+    			'&markers=color:blue%7Clabel:B%7C'.$this->parameters["startLocation"].
+    			'&markers=color:green%7Clabel:E%7C.'. $this->parameters["endLocation"].
+    			'&sensor=false';
+    	$arr = ["url" => $url];
+    	echo json_encode($arr);
+    }
+    
+    
 }
 
 ?>
