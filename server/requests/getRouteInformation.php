187ecbfd2791d9af72d219a231d299ebe1e22baa
Janeth <janeth.moranc@gmail.com>
2013-07-27 18:59:13 -0700

    getRouteInformation

--- server/requests/getRouteInformation.php
+++ server/requests/getRouteInformation.php
@@ -8,6 +8,7 @@ class routeInformationGetter
     public function __construct ()
     {
         $this->parameters = $_GET;
+        
     }
 
     public function returnResponse ()
@@ -20,24 +21,98 @@ class routeInformationGetter
         }
     }
 
+
+    //need startTime, startLocation, endLocation
+    //assume transportMode is 
+    
+	$url = 'http://maps.googleapis.com/maps/api/directions/json?';
     public function getCaltrainInformation ()
     {
-
+	  if($this->parameters["timeType"] == "startTime")
+	  { 
+	    $url_request = $url.
+	    	'departure_time='$this->parameters["startTime"].
+			'&origin='.$this->parameters["startLocation"].'|Caltrain'.
+			'&destination='.$this->paramters["endLocation"].'|Caltrain'.
+			'&mode=transit'.
+			'&sensor=false'; 
+		
+	  }
+	  else // "endTime"
+	  {
+	    $url_request = $url.
+	    	'arrival_time='.$this->parameters["endTime"].
+			'&origin='.$this->parameters["startLocation"].'|Caltrain'.
+			'&destination='.$this->parameters["endLocation"].'|Caltrain'
+			'&mode=transit'.
+			'&sensor=false'; 
+	  }
     }
 
     public function getBARTInformation ()
     {
-
+	  if($this->parameters["timeType"] == "startTime")
+	  { 
+		$url_request = $url.
+			'departure_time='.$this->parameters["startTime"].
+			'&origin='.$this->parameters["startLocation"].'|Bart'
+			'&destination='.$this->paramters["endLocation"].'|Bart'.
+			'&mode=transit'.
+			'&sensor=false'; 
+	  }
+	  else
+	  {
+		$url_request = $url.
+			'arrival_time='.$this->parameters["endTime"].
+			'&origin='.$this->parameters["startLocation"].'|Bart'
+			'&destination='.$this->paramters["endLocation"].'|Bart'.
+			'&mode=transit'.
+			'&sensor=false';	  
+	  }
     }
 
     public function getMUNIRailInformation ()
     {
-
+	  if($this->parameters["timeType"] == "startTime")
+	  {
+	  	$url_request = $url.
+			'departure_time='.$this->parameters["startTime"].
+			'&origin='.$this->parameters["startLocation"].'|Muni'.
+			'&destination='.$this->paramters["endLocation"].'|Muni'.
+			'&mode=transit'.
+			'&sensor=false'; 
+	  }
+	  else
+	  {
+	  	$url_request = $url.
+			'arrival_time='.$this->parameters["endTime"].
+			'&origin='.$this->parameters["startLocation"].'|Muni'.
+			'&destination='.$this->paramters["endLocation"].'|Muni'.
+			'&mode=transit'.
+			'&sensor=false'; 
+	  }
     }
 
     public function getWalkInformation ()
     {
-
+	  if($this->parameters["timeType"] == "startTime")
+	  {
+	  	$url_request = $url.
+			'departure_time='.$this->parameters["startTime"].
+			'&origin='.$this->parameters["startLocation"].
+			'&destination='.$this->paramters["endLocation"].
+			'&mode=walking'.
+			'&sensor=false'; 
+	  }
+	  else
+	  {
+	  	$url_request = $url.
+			'arrival_time='.$this->parameters["endTime"].
+			'&origin='.$this->parameters["startLocation"].
+			'&destination='.$this->paramters["endLocation"].
+			'&mode=walking'.
+			'&sensor=false'; 
+	  }
     }
 }
 
